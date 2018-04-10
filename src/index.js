0b3b63b65d86a42768b14bd1efe55599055f236c
rodrigovallades <rodrigo.vallades@gmail.com>
2018-04-10 00:49:40 -0300

    Finishing touches

--- src/index.js
+++ src/index.js
@@ -3,30 +3,30 @@ import { App } from './App';
 function init() {
 
   const tableInfo = [
-    ['something@somewhere.com', 'Joe', 'Smith Jr.', 'Important people', '781-633-2222', 120],
-    ['anywhere@anywhere.com', 'Jane', 'Doe', 'Not so important people', '871-336-2222', 2],
+    ['something@somewhere.com', 'Joe', 'Smith Jr.', 'Important', '781-633-2222', 120],
+    ['anywhere@anywhere.com', 'Jane', 'Doe', 'Not important', '871-336-2222', 2],
     ['rodrigo@vallades.com', 'Rodrigo', 'Vallades', 'VIP', '555-111-0000', 0],
-    ['bny@bank.com', 'Nivea', 'Men', 'Important people', '--', 0],
+    ['bny@bank.com', 'Nivea', 'Men', 'Important', '--', 0],
     ['react@facebook.com', 'React', 'Redux', 'VIP', '111-546-1264', 8000],
     ['es6@javascript.com', 'Rodrigo', 'Vallades', 'VIP', '654-319-7987', 5000],
-    ['something@somewhere.com', 'Joe', 'Smith Jr.', 'Important people', '781-633-2222', 1000],
-    ['anywhere@anywhere.com', 'Jane', 'Doe', 'Not so important people', '871-336-2222', 500],
+    ['something@somewhere.com', 'Joe', 'Smith Jr.', 'Important', '781-633-2222', 1000],
+    ['anywhere@anywhere.com', 'Jane', 'Doe', 'Not important', '871-336-2222', 500],
     ['elsewhere@elsewhere.com', 'Rodrigo', 'Vallades', 'VIP', '555-111-000', 5000],
-    ['something@somewhere.com', 'Joe', 'Smith Jr.', 'Important people', '781-633-2222', 120],
-    ['anywhere@anywhere.com', 'Jane', 'Doe', 'Not so important people', '871-336-2222', 2],
+    ['something@somewhere.com', 'Joe', 'Smith Jr.', 'Important', '781-633-2222', 120],
+    ['anywhere@anywhere.com', 'Jane', 'Doe', 'Not important', '871-336-2222', 2],
     ['elsewhere@elsewhere.com', 'Rodrigo', 'Vallades', 'VIP', '555-111-0000', 0],
-    ['bny@bank.com', 'Nivea', 'Men', 'Important people', '--', 0],
+    ['bny@bank.com', 'Nivea', 'Men', 'Important', '--', 0],
     ['react@facebook.com', 'React', 'Redux', 'VIP', '111-546-1264', 8000],
     ['es6@javascript.com', 'Rodrigo', 'Vallades', 'VIP', '654-319-7987', 5000],
-    ['something@somewhere.com', 'Joe', 'Smith Jr.', 'Important people', '781-633-2222', 1000],
-    ['anywhere@anywhere.com', 'Jane', 'Doe', 'Not so important people', '871-336-2222', 500],
+    ['something@somewhere.com', 'Joe', 'Smith Jr.', 'Important', '781-633-2222', 1000],
+    ['anywhere@anywhere.com', 'Jane', 'Doe', 'Not important', '871-336-2222', 500],
     ['elsewhere@elsewhere.com', 'Rodrigo', 'Vallades', 'VIP', '555-111-000', 5000]
   ];
 
   const app = new App({
     people: tableInfo
   });
-  
+
   app.render();
 }
 
