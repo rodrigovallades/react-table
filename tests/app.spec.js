3a7f8c77c5ab18ee4c2b177b115ce37335e96926
rodrigovallades <rodrigo.vallades@gmail.com>
2018-04-10 21:16:59 -0300

    Testing if app has been instantiated

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,12 +1,23 @@
-var expect = require('chai').expect;
-var App = require('../src/App/App.js')/
+import { expect } from 'chai';
+import { App } from '../src/App/App';
 
 describe('App', function(){
+
+  let appInstance;
+
+  beforeEach(function() {
+    appInstance = new App();
+  })
 
   describe('Smoke tests', function() {
 
     it('should exist an App module', function() {
-      expect(App).to.exist;
+      expect(appInstance).to.exist;
     })
+
+    it('should exist a `render` method', function() {
+      expect(appInstance.render).to.exist;
+    })
+
   });
 });
